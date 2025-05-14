7c4d97f15bd4f5f2ef528df6952402101fd1a91b
Levin Bänninger <l.baenninger@icloud.com>
2025-05-14 09:51:08 +0200

    feature: integrate MongoDB with API, add configuration and database listing functionality

--- api/Program.cs
+++ api/Program.cs
@@ -1,6 +1,31 @@
+using MongoDB.Driver;
+using Microsoft.Extensions.Options;
+using Api;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<DatabaseSettings>(
+    builder.Configuration.GetSection("DatabaseSettings"));
+
+builder.Services.AddSingleton<IMongoClient>(sp => {
+    var settings = sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+    return new MongoClient(settings.ConnectionString);
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/databases", async (IMongoClient mongoClient) =>
+{
+    try
+    {
+        var databases = await mongoClient.ListDatabaseNamesAsync();
+        var databaseList = await databases.ToListAsync();
+        return Results.Ok(databaseList);
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem(ex.Message);
+    }
+});
 
 app.Run();
---
+++ api/Settings/DatabaseSettings.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Api;
+
+public class DatabaseSettings
+{
+    public string ConnectionString { get; set; } = "";
+}

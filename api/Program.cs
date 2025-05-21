058a7befe07b7525fd0e072011521d3a84aeb6e7
Levin Bänninger <l.baenninger@icloud.com>
2025-05-21 08:43:04 +0200

    fix: update MongoDB connection settings and enhance data persistence in compose file

--- api/Program.cs
+++ api/Program.cs
@@ -1,23 +1,18 @@
 using MongoDB.Driver;
 using Microsoft.Extensions.Options;
-using Api;
+using Api.Settings;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.Configure<DatabaseSettings>(
-    builder.Configuration.GetSection("DatabaseSettings"));
-
-builder.Services.AddSingleton<IMongoClient>(sp => {
-    var settings = sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
-    return new MongoClient(settings.ConnectionString);
-});
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
 
 var app = builder.Build();
 
-app.MapGet("/databases", async (IMongoClient mongoClient) =>
+app.MapGet("/check", async (IOptions<DatabaseSettings> options) =>
 {
     try
     {
+        var mongoClient = new MongoClient(options.Value.ConnectionString);
         var databases = await mongoClient.ListDatabaseNamesAsync();
         var databaseList = await databases.ToListAsync();
         return Results.Ok(databaseList);
